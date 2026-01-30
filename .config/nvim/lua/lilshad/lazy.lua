d22994ea283ff54db6e1460226cdfb9fc2b0208c
Maxime Bellet <maxime.bellet@wit.fr>
2026-01-30 17:29:47 +0100

    feat: add mason (wip lsp)

--- .config/nvim/lua/lilshad/lazy.lua
+++ .config/nvim/lua/lilshad/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "lilshad.plugins" },
+    { import = "lilshad.plugins.lsp" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
