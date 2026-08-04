fdb25f065970586a1311531a92af1fceeaedd1d8
Maxime Bellet <maxime.bellet@wit.fr>
2026-08-04 15:04:16 +0200

    feat: add which keys and todo-comments

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,37 @@
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        preset = "helix",
+        delay = function(ctx)
+            return ctx.plugin and 0 or 250
+        end,
+        icons = {
+            mappings = true,
+            colors = true,
+        },
+        spec = {
+            {
+                mode = { "n", "v" },
+                { "<leader>b", group = "buffer" },
+                { "<leader>f", group = "find" },
+                { "<leader>g", group = "git" },
+                { "<leader>s", group = "search" },
+                { "<leader>t", group = "tab" },
+                { "<leader>u", group = "ui / toggle" },
+                { "<leader>w", group = "window" },
+                -- Neovim 0.12 binds grn/gra/grr/gri/grt/grx here (:h lsp-defaults).
+                { "gr", group = "lsp" },
+            },
+        },
+    },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer keymaps (which-key)",
+        },
+    },
+}
